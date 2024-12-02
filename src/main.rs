e9a412a38ab7e33cb8aca82984332000f20fc359
Morganballat <mgp.ballat@gmail.com>
2024-12-02 17:23:42 +0100

    user input for the country name

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main(){
-    println!("Hello, world!");
+use std::io;
+
+fn main() {
+    println!("renseignez le nom d'un pays en anglais pour obtenir des informations sur ce dernier");
+
+    let mut country_name = String::new();
+
+    io::stdin().read_line(&mut country_name).expect("Failed to read line");
+
+    println!("Le pays est le suivant: {}", country_name);
+
+
 }
